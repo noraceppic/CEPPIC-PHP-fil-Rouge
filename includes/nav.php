ce53b1bb450565f6598245bcfd47007f3f07a947
noraceppic <messanora86@gmail.com>
2022-06-14 11:55:19 +0200

    ajout icon login out

--- includes/nav.php
+++ includes/nav.php
@@ -10,14 +10,20 @@ session_start();
             if(isset($_SESSION['loginUser'])){
                 ?>
                 <span class="login">
-                    <li><a href="index.php?page=logout">Logout</a></li>
-                    <li><strong><?=$_SESSION['loginUser'] ?> </strong></li>
+                    <li><a href="index.php?page=logout"><i class="fa fa-sign-out"></i></a></li>
+                    
+                    <?php
+
+                      echo strstr($_SESSION['loginUser'], '@', true);
+
+                    ?>
 
                 </span>
                 <?php } else { ?>
                     <li><a href="index.php?page=inscription">inscription</a></li>
-                    <li><a href="index.php?page=login">Login</a></li>
-    
+                    <span class="login">
+                    <li><a href="index.php?page=login"><i class="fa fa-user"></i></a></li>
+                    </span>
             <?php } ?>
 
         </ul>
